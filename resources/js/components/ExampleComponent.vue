ff7b9ea245900b276d4e1e3dd629029367affc78
dyegoalves <dyego.santos.alves@gmail.com>
2019-08-29 06:33:34 -0400

    Remove space files

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,26 +1,21 @@
-
-'<script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
-    }
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
 
 <template>
-    <div id='ex01' class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-body">
-                        Eu sou um exemplo de componente - VUE js
-                    </div>
-                </div>
-            </div>
+  <div id="ex01" class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card card-default">
+          <div class="card-body">Eu sou um exemplo de componente - VUE js</div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
--- resources/js/fixmenu.js
+++ resources/js/fixmenu.js
@@ -2,16 +2,16 @@
 var url = window.location;
 // for sidebar menu entirely but not cover treeview
 $("ul.sidebar-menu a")
-    .filter(function() {
-        return this.href == url;
-    })
-    .parent()
-    .addClass("active");
+.filter(function() {
+    return this.href == url;
+})
+.parent()
+.addClass("active");
 
 // for treeview
 $("ul.treeview-menu a")
-    .filter(function() {
-        return this.href == url;
-    })
-    .parentsUntil(".sidebar-menu > .treeview-menu")
-    .addClass("active");
+.filter(function() {
+    return this.href == url;
+})
+.parentsUntil(".sidebar-menu > .treeview-menu")
+.addClass("active");
