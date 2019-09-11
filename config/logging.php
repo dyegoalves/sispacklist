26d7d2af80de395b5aeac837bc0c9ebb20daa849
dyegoalves <dyego.santos.alves@gmail.com>
2019-09-11 12:42:34 -0400

    Add - error page laravel ignition

--- config/logging.php
+++ config/logging.php
@@ -36,7 +36,8 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+             'channels' => ['daily', 'flare'],
+               'ignore_exceptions' => false,
         ],
 
         'single' => [
--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,45 +1,44 @@
-
 @extends('layout.master')
 @section('title', 'Inicio')
 @section('conteudo')
- <!-- =============================================== -->
-  <!-- Inicio do Conteudo -->
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-       <span class="fa fa-home"></span> Home
-        <small> - Inicio do Sistema</small>
-      </h1>
-    </section>
-    <!-- Main content -->
-    <section class="content">
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Nome OP</h3>
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
+<!-- =============================================== -->
+<!-- Inicio do Conteudo -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      <span class="fa fa-home"></span> Home
+      <small> - Inicio do Sistema</small>
+    </h1>
+  </section>
+  <!-- Main content -->
+  <section class="content">
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <h3 class="box-title">Nome OP</h3>
+        <div class="box-tools pull-right">
+          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+            <i class="fa fa-minus"></i></button>
+          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+            <i class="fa fa-times"></i></button>
         </div>
-        <div class="box-body">
-          1* Sistema criado para conferencia de CKD para o processo produtido de PLACAS SMT
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Descrição Operação
-        </div>
-        <!-- /.box-footer-->
       </div>
-      <!-- /.box -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
- <!-- Fim do Conteudo -->
+      <div class="box-body">
+        1* Sistema criado para conferencia de CKD para o processo produtido de PLACAS SMT
+
+      </div>
+      <!-- /.box-body -->
+      <div class="box-footer">
+        Descrição Operação
+      </div>
+      <!-- /.box-footer-->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<!-- Fim do Conteudo -->
 @endsection
